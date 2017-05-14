7d46da846798426ab4097d3aa7a95c2873004791
Dan Metzler <dmetzler@ucsd.edu>
2017-05-13 23:32:04 -0700

    Completed feed loading

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -10,7 +10,48 @@ function buildListeners() {
 // do login process
 function authenticate() {
     // the InAppBrowser window used for authentication
-    https://api.instagram.com/oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=token
-    var auth = window.open("https://api.instagram.com/oauth/authorize/?client_id=" + KEYS.CLIENT_ID + "&redirect_uri=REDIRECT-URI&response_type=token", "_blank");
+    var auth = window.open("https://api.instagram.com/oauth/authorize/?client_id=" + KEYS.CLIENT_ID + "&redirect_uri=https://www.instagram.com&response_type=token", "_blank", "location=no");
+    // add listener for load
+    auth.addEventListener("loadstop", function(e) {
+        // url string passed by event
+        var url = e.url;
+        // access token pulled from url
+        var token = url.split("#access_token=")[1];
+        // instagram access token
+        KEYS.ACCESS_TOKEN = token;
+        // close window
+        auth.close();
+        // request feed information from the API
+        $.get("https://api.instagram.com/v1/users/self/media/recent/?access_token=" + KEYS.ACCESS_TOKEN + "&count=18", function(response) {
+            parseFeed(response.data);
+        });
+    });
+}
+
+// load a user's feed into the UI
+function parseFeed(data) {
+    // number of rows parsed already
+    var rowCount = 1;
+    // create the first row
+    $(".feed-wrapper").append("<div class='feed-row' id='feed-row-1'></div>");
+    // add each image to a row
+    for(var i = 0; i < data.length; i++) {
+
+        // add the image
+        $("#feed-row-" + rowCount).append("<div class='feed-item' id='feed-item-" + i + "'></div>");
+        $("#feed-item-" + i).css("background-image", "url(" + data[i].images.standard_resolution.url + ")");
+        // check if the row is full
+        if((i + 1) % 3 == 0 && i + 1 < data.length) {
+            rowCount++;
+            $(".feed-wrapper").append("<div class='feed-row' id='feed-row-" + rowCount + "'></div>");
+        }
+    }
+    // reveal the feed UI
+    loadFeed();
+}
+
+function loadFeed() {
+    $("#login").css("display", "none");
+    $("#feed").css("display", "block");
 }
 
