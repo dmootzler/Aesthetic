b7757a0a550ed1c692b31ae3596cb4a4b4657dd4
Dan Metzler <dmetzler@ucsd.edu>
2017-05-15 00:05:06 -0700

    Finished first launch of app

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -1,11 +1,99 @@
+// key-value storage
+var storage = window.localStorage;
+// preview button image
+var preview;
+
 $(function() {
     FastClick.attach(document.body);
     buildListeners();
+    if(storage.getItem("access_token")) {
+        callAPI(storage.getItem("access_token"));
+    }
+    preview = $("#feed-item-0").css("background-image");
 });
 
 // add listeners to buttons
 function buildListeners() {
+    // add click listener to connect to IG button
     $("#login-btn").click(authenticate);
+    // add click listener to preview button
+    $("#feed-item-0").click(previewPic);
+    // add click listener to help button for preview
+    $("#help-preview").click(function() {
+        closeHelp();
+        previewPic();
+    });
+    // add click listener to close help button
+    $("#help-close").click(function() {
+        closeHelp();
+    });
+    // add click listener to logout button
+    $("#help-logout").click(function() {
+        closeHelp();
+        logout();
+    });
+    
+    // add swipe listeners to feed
+    var feed = document.getElementById("feed");
+    Hammer(feed).on("swiperight", help);
+}
+
+// display an error message when unclickable items are clicked
+function unclickable() {
+    navigator.notification.alert("myAesthetic is only for previewing pictures before you post. Use the official Instagram app to access this feature and many others!", null, "Sorry!", "Back to myAesthetic");
+}
+
+// logout the current user and return to the login screen
+function logout() {
+    // remove the access token from storage
+    storage.removeItem("access_token");
+    // remove the access token from memory
+    KEYS.ACCESS_TOKEN = null;
+    $("#feed-item-0").css("background-image", preview);
+    $("#login").css("display", "flex");
+    $("#feed").css("display", "none");
+    $(".feed-wrapper .remove").remove();
+}
+
+// position the elements of the help window
+function initHelp() {
+    // position the preview guidelines
+    $("#help-preview").offset($("#feed-item-0").offset());
+    // add swipe listener to help window
+    var help = document.getElementById("help");
+    Hammer(help).on("swipeleft", closeHelp);
+}
+
+// display the help info
+function help() {
+    // show the help screen
+    $("#help").css("display", "flex");
+    // initialize the help screen
+    initHelp();
+}
+
+// close the help info
+function closeHelp() {
+    $("#help").css("display", "none");
+}
+
+// preview a picture within the feed
+function previewPic() {
+    // load picture into preview screen on success
+    function onSuccess(imageURI) {
+        $("#feed-item-0").css("background-image", "url(" + imageURI + ")");        
+    }
+    
+    // alert on failure with cause
+    function onFail(message) {
+        
+    }
+    // request to take a picture
+    navigator.camera.getPicture(onSuccess, onFail, {
+        quality: 100,
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+    }); 
 }
 
 // do login process
@@ -22,14 +110,22 @@ function authenticate() {
         KEYS.ACCESS_TOKEN = token;
         // close window
         auth.close();
-        // request feed information from the API
-        $.get("https://api.instagram.com/v1/users/self/media/recent/?access_token=" + KEYS.ACCESS_TOKEN + "&count=18", function(response) {
-            parseFeed(response.data);
-        });
-        // request profile information from API
-        $.get("https://api.instagram.com/v1/users/self/?access_token=" + KEYS.ACCESS_TOKEN, function(response) {
-            parseProfile(response.data);
-        });
+        // store the access token in memory
+        storage.setItem("access_token", token);
+        // retrieve posts and user info from API
+        callAPI(token);
+    });
+}
+
+// retrieve posts and user info from API
+function callAPI(token) {
+    // request feed information from the API
+    $.get("https://api.instagram.com/v1/users/self/media/recent/?access_token=" + KEYS.ACCESS_TOKEN + "&count=18", function(response) {
+        parseFeed(response.data);
+    });
+    // request profile information from API
+    $.get("https://api.instagram.com/v1/users/self/?access_token=" + KEYS.ACCESS_TOKEN, function(response) {
+        parseProfile(response.data);
     });
 }
 
@@ -37,17 +133,15 @@ function authenticate() {
 function parseFeed(data) {
     // number of rows parsed already
     var rowCount = 1;
-    // create the first row
-    $(".feed-wrapper").append("<div class='feed-row' id='feed-row-1'><div class='feed-item' id='feed-item-0'></div></div>");
     // add each image to a row
     for(var i = 1; i < data.length + 1; i++) {
         // add the image
-        $("#feed-row-" + rowCount).append("<div class='feed-item' id='feed-item-" + i + "'></div>");
+        $("#feed-row-" + rowCount).append("<div class='feed-item remove unclickable' id='feed-item-" + i + "'></div>");
         $("#feed-item-" + i).css("background-image", "url(" + data[i - 1].images.standard_resolution.url + ")");
         // check if the row is full
         if((i + 1) % 3 == 0 && i < data.length) {
             rowCount++;
-            $(".feed-wrapper").append("<div class='feed-row' id='feed-row-" + rowCount + "'></div>");
+            $(".feed-wrapper").append("<div class='feed-row remove' id='feed-row-" + rowCount + "'></div>");
         }
     }
     // reveal the feed UI
@@ -64,10 +158,20 @@ function parseProfile(data) {
     $("#profile-name").html(data.full_name);
     $("#profile-bio").html(data.bio);
     
+    $("#help-username").html(data.username);
+    $("#help-name").html(data.full_name);
 }
 
 function loadFeed() {
+    // hide the login page
     $("#login").css("display", "none");
+    // reveal the feed preview
     $("#feed").css("display", "flex");
+    
+    // add click listeners to all unclickable items
+    $(".unclickable").click(unclickable);
+    
+    // create help popup
+    navigator.notification.alert("Swipe right at any time to access the help menu.", null, "Help Menu", "Back to myAesthetic");
 }
 
